9bb3add34cd994e3b0850cd42dc4787483232767
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2024-03-05 00:49:48 -0300

    Add files via upload

---
+++ ArquivoDeBanda/ArquivoDeBanda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string banda, instrumentos;
+        int discos, shows, musicas;
+
+        Console.WriteLine("Digite o nome da banda: ");
+        banda = Console.ReadLine();
+
+        Console.WriteLine("Quais instrumentos a banda possui? ");
+        instrumentos = Console.ReadLine();
+
+        Console.WriteLine("Quantos discos a banda já gravou? ");
+        discos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quantos shows a banda já fez?");
+        shows = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quantas musicas a banda tem? ");
+        musicas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Conclusao: ");
+
+        Console.WriteLine("Nome da banda: " + banda);
+        Console.WriteLine("Instrumentos: " + instrumentos);
+        Console.WriteLine("Discos lancados: " + discos);
+        Console.WriteLine("Quantidade de shows: " + shows);
+        Console.WriteLine("Quantidade de musicas: " + musicas);
+    }
+}
